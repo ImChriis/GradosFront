2534106a89caf81649558edc86534fa23dcd8d74
Clases <clases@labcomp.urbe.edu>
2026-01-31 12:52:48 -0400

    interfaces listas

--- src/app/pages/act-places/act-places.component.ts
+++ src/app/pages/act-places/act-places.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { ActsService } from '../../@core/services/acts.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-act-places',
-  imports: [],
+  imports: [
+    CommonModule,
+    TableModule,
+    InputTextModule
+  ],
   templateUrl: './act-places.component.html',
   styleUrl: './act-places.component.scss'
 })
 export class ActPlacesComponent {
+  private actsService = inject(ActsService);
+  isAdding = false;
+  isEnabled = false;
+  selectedAct = false;
+  acts$!: Observable<any[]>;
+
+  onSelect(){
+
+  }
+
+  onAdd(){
+
+  }
+
+  onSave(){
+
+  }
+
+  onDelete(){
+
+  }
+
+  onCancel(){
+
+  }
+
 
 }
+
