217da0736a04fb4f69b57952edc098be8498cafa
Clases <clases@labcomp.urbe.edu>
2026-01-31 15:24:07 -0400

    services interfaces

--- src/app/@core/services/acts.service.ts
+++ src/app/@core/services/acts.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Act } from '../models/act.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActsService {
+  private api: string = environment.api;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getAllActs(){{
+    return this.http.get<Act[]>(`${this.api}/acts`).pipe(
+      map((res: Act[] = []) => {
+       console.log("Raw response: ", res);
+       const items = res ?? [];
+       const sorted = res.slice().sort((a: any, b: any) => (b.id ?? 0) - (a.id ?? 0)) ;
+       return sorted.map((act: any) => ({ ...act }));
+      })
+    )
+  }}
 }
--- src/app/@core/services/institutions.service.ts
+++ src/app/@core/services/institutions.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Institution } from '../models/institution.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,13 @@ export class InstitutionsService {
   private http = inject(HttpClient);
 
   getAllInstitutions(){
-    return this.http.get<any[]>(`${this.api}/institutions`);
+    return this.http.get<Institution[]>(`${this.api}/institutions`).pipe(
+      map((res: Institution[] = []) => {
+        console.log("Raw Response: ", res);
+        const items = res ?? [];
+        const sorted = items.slice().sort((a: any, b: any) => (b.id ?? 0) - (a.id ?? 0));
+        return sorted.map((institution: any) => ({ ...institution }));
+      })
+    )
   }
 }
