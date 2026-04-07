ef61eebec14305c51b95550cab554a31b7526fa8
Chris <christophermedinabarroso@gmail.com>
2026-04-07 16:52:08 -0400

    cambios layout

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     prod: false,
-    // api: 'http://localhost:3000', //development
-    api: 'https://straightforward-rosalyn-grados-788a8b0a.koyeb.app' //production
+    api: 'http://localhost:3000', //development
+    // api: 'https://straightforward-rosalyn-grados-788a8b0a.koyeb.app' //production
 }
 
