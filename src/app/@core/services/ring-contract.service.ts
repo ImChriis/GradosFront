b9981c63b11e25ec69a70497111c750be4278f38
Chris <christophermedinabarroso@gmail.com>
2026-07-28 16:25:39 -0400

    rings

--- src/app/@core/services/ring-contract.service.ts
+++ src/app/@core/services/ring-contract.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RingContractService {
+  private http = inject(HttpClient);
+  private api: string = environment.api;
 
-  constructor() { }
+  getRingContract() {
+  return this.http.get(`${this.api}/ringContracts`).pipe(
+    tap((data) => {
+      console.log(data);
+    })
+  )
+  }
 }
--- src/app/pages/ring-contract/ring-contract.component.ts
+++ src/app/pages/ring-contract/ring-contract.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { LoaderComponent } from '../../@core/components/loader/loader.component';
+import { RingContractService } from '../../@core/services/ring-contract.service';
+import { Observable } from 'rxjs';
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'app-ring-contract',
@@ -12,11 +15,18 @@ import { LoaderComponent } from '../../@core/components/loader/loader.component'
     TableModule,
     InputTextModule,
     ReactiveFormsModule,
-    LoaderComponent
+    LoaderComponent,
+    TabsModule
   ],
   templateUrl: './ring-contract.component.html',
   styleUrl: './ring-contract.component.scss'
 })
-export class RingContractComponent {
+export class RingContractComponent implements OnInit {
+  private ringContractService = inject(RingContractService);
+  contracts$!: Observable<any>;
   isLoading = signal(false);
+
+  ngOnInit() {
+    this.contracts$ = this.ringContractService.getRingContract();
+  }
 }
